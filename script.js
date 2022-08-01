b299c8ce3ce556b87329d37676c4070a9a55449d
jeova paiva <jeovappaiva@gmail.com>
2022-07-31 22:56:42 -0300

    a007

---
+++ script.js
@@ -0,0 +1,67 @@
+//prompt("Qual é o seu nome?");
+
+//console.log("Jeova paiva!");
+
+//let nome = "jeova";
+//console.log(nome);
+
+//let idade = 27.5
+//console.log(idade);
+
+//nome = "paiva";
+//idade = 25;
+//console.log(nome);
+//console.log(idade);
+
+//console.log("Olá! meu nome é",nome, "e eu tenho", idade, "anos.");
+
+//const temPets = true
+//let nomeDoPet = "dog"
+
+//const luizTemPets = false
+//let nomeDoPetDeLuiz = null 
+
+// const money = 1256.35;
+// const idade = 26;
+// const graus = -125;
+// console.log(typeof money);
+// console.log(typeof idade);
+// console.log(typeof graus);
+
+//const nomestring = prompt("Qual o seu nome?"); //Quando se coloca um "+" ante do pronpt, aparece um number em mvez de uma string.
+//const idade = +prompt("Qual sua idade?");
+
+//console.log(typeof nome, typeof idade);
+//console.log( nomestring, idade);
+
+
+//const idadeNumber = +prompt("Qual a sua idade?");
+//const idadestring = idadeNumber.toString();
+
+
+//console.log(typeof idadestring);
+
+//let A = 30; //tracar o valor de3 A por B, tem que usar uma outra variavel como a C 
+//let B = 20;
+//let C;
+
+//C = A;
+//A = B;
+//B = C;
+
+//console.log("A", A);
+//console.log("B", B);
+//console.log("C", C);
+
+let A = 20;
+let B = 30;
+let C = null;
+
+C = A+B; // exemplo da aula.
+B = C-B;
+A = C-A;
+
+console.log("valor A", A,"valor B", B);
+
+
+
